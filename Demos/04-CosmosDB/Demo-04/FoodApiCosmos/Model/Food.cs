d57cec4fd735ebd2bcf0962c80ab81e48ad63dd9
Alexander Pajer <alexander.pajer@integrations.at>
2021-02-07 18:24:41 +0100

    foodcosmos-removed

--- Demos/04-CosmosDB/Demo-04/FoodApiCosmos/Model/Food.cs
+++
@@ -1,9 +0,0 @@
-namespace CosmosEF
-{
-    public class FoodItem{
-        public int id { get; set; }
-        public string name { get; set; }
-        public decimal price { get; set; }
-        public int calories { get; set; }
-    }
-}
--- Demos/04-CosmosDB/Demo-04/readme.md
+++
@@ -1,1 +0,0 @@
-# Cosmos DB with Entity Framework
