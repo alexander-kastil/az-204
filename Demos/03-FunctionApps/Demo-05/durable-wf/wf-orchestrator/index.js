8f8d2127d01cecb480d88d7019704ad683447194
Alexander Pajer <alexander.pajer@integrations.at>
2021-05-01 09:37:41 +0200

    durable funct scaffold

---
+++ Demos/03-FunctionApps/Demo-05/durable-wf/wf-orchestrator/index.js
@@ -0,0 +1,10 @@
+﻿const df = require("durable-functions");
+
+module.exports = df.orchestrator(function* (context) {
+  const outputs = [];
+
+  outputs.push(yield context.df.callActivity("Approval", "Approved"));
+  outputs.push(yield context.df.callActivity("Approval", "Rejected"));
+
+  return outputs;
+});
