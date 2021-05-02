4c6d7d0373f69d19ceb8c23cd705647015d5734e
Alexander Pajer <alexander.pajer@integrations.at>
2021-05-02 09:33:16 +0200

    inital

--- Demos/03-FunctionApps/Demo-05/durable-wf/wf-orch/index.js
+++ Demos/03-FunctionApps/Demo-05/durable-wf/wf-orch/index.js
@@ -1,10 +1,27 @@
 ﻿const df = require("durable-functions");
+const moment = require("moment");
 
 module.exports = df.orchestrator(function* (context) {
   const outputs = [];
 
   outputs.push(yield context.df.callActivity("Approval", "Approved"));
   outputs.push(yield context.df.callActivity("Approval", "Rejected"));
+
+  // const deadline = moment.utc(context.df.currentUtcDateTime).add(20, "s");
+  // const activityTask = context.df.waitForExternalEvent("Approval");
+  // const timeoutTask = context.df.createTimer(deadline.toDate());
+
+  // const winner = yield context.df.Task.any([activityTask, timeoutTask]);
+  // if (winner === activityTask) {
+  //   outputs.push(yield context.df.callActivity("Approval", "Approved"));
+  // } else {
+  //   outputs.push(yield context.df.callActivity("Escalation", "Head of department"));
+  // }
+
+  // if (!timeoutTask.isCompleted) {
+  //   // All pending timers must be complete or canceled before the function exits.
+  //   timeoutTask.cancel();
+  // }
 
   return outputs;
 });
