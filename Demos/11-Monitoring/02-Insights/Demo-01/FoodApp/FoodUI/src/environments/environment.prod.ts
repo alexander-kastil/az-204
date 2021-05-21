3967b91e44bb3f6b01dd888a81224f0e963a036a
Alexander Pajer <alexander.pajer@integrations.at>
2021-05-21 13:10:25 +0200

    dood app deployed

--- Demos/11-Monitoring/02-Insights/Demo-01/FoodApp/FoodUI/src/environments/environment.prod.ts
+++ Demos/11-Monitoring/02-Insights/Demo-01/FoodApp/FoodUI/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiurl: "https://foodapi-007.azurewebsites.net/api/",
+  apiurl: "https://foodapi-025.azurewebsites.net/api/",
   appInsights: {
-    instrumentationKey: "3b509da2-ddbc-4c0e-b1c5-f78233fe41ea",
+    instrumentationKey: "05cfdd0c-ffb8-46de-854d-a2a29e7c39cc",
   },
 };
--- Demos/11-Monitoring/02-Insights/Demo-01/FoodApp/FoodUI/src/environments/environment.ts
+++ Demos/11-Monitoring/02-Insights/Demo-01/FoodApp/FoodUI/src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   apiurl: "https://localhost:5001/api/",
   appInsights: {
-    instrumentationKey: "3b509da2-ddbc-4c0e-b1c5-f78233fe41ea",
+    instrumentationKey: "05cfdd0c-ffb8-46de-854d-a2a29e7c39cc",
   },
 };
