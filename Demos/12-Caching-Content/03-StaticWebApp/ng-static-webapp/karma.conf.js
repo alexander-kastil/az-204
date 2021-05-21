706ac173237405fe983c7bf64cc29a44cfb18d7d
Alexander Pajer <alexander.pajer@integrations.at>
2021-05-21 08:41:23 +0200

    static webapp ng12 replacement

--- Demos/12-Caching-Content/03-StaticWebApp/ng-static-webapp/karma.conf.js
+++ Demos/12-Caching-Content/03-StaticWebApp/ng-static-webapp/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ng-static-web-app'),
+      dir: require('path').join(__dirname, './coverage/ng-static-webapp'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- Demos/12-Caching-Content/03-StaticWebApp/ng-static-webapp/src/app/app.component.spec.ts
+++ Demos/12-Caching-Content/03-StaticWebApp/ng-static-webapp/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ng-static-web-app'`, () => {
+  it(`should have as title 'ng-static-webapp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ng-static-web-app');
+    expect(app.title).toEqual('ng-static-webapp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ng-static-web-app app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('ng-static-webapp app is running!');
   });
 });
