c28cc904a0b7f5598c4990fbd849a6ad97bbeaf6
Alexander Pajer <alexander.pajer@integrations.at>
2021-06-10 08:55:44 +0200

    docs

--- Demos/06-Identity/01-Authentication/Demo-01/token-flow-node/index.js
+++ Demos/06-Identity/01-Authentication/Demo-01/token-flow-node/index.js
@@ -1,48 +1,48 @@
 async function doAuth() {
-  //Sharepoint Tenant
-  const spTenant = "integrationsonline";
+    //Sharepoint Tenant
+    const spTenant = "integrationsonline";
 
-  const config = {
-    auth: {
-      clientId: "eeb155cb-d4c6-4864-9184-cf10a6e02715",
-      authority: "https://login.microsoftonline.com/common/",
-      redirectUri: "http://localhost:8080",
-    },
-  };
+    const config = {
+        auth: {
+            clientId: "eeb155cb-d4c6-4864-9184-cf10a6e02715",
+            authority: "https://login.microsoftonline.com/d92b247e-90e0-4469-a129-6a32866c0d0a/",
+            redirectUri: "http://localhost:8080",
+        },
+    };
 
-  //Creadte MSAL App with Scope to read User Profile
-  const client = new Msal.UserAgentApplication(config);
-  const scopes = {
-    scopes: ["user.read"],
-  };
+    //Creadte MSAL App with Scope to read User Profile
+    const client = new Msal.UserAgentApplication(config);
+    const scopes = {
+        scopes: ["user.read"],
+    };
 
-  //Login -> Get ID Token
-  const loginResponse = await client
-    .loginPopup(scopes)
-    .then((loginResponse) => {
-      console.log("id_token acquired at: " + new Date().toString());
-      console.log("LoginResponse", loginResponse);
+    //Login -> Get ID Token
+    const loginResponse = await client
+        .loginPopup(scopes)
+        .then((loginResponse) => {
+            console.log("id_token acquired at: " + new Date().toString());
+            console.log("LoginResponse", loginResponse);
 
-      if (client.getAccount()) {
-        console.log("Account", client.getAccount());
-      }
-    })
-    .catch((error) => {
-      console.log(error);
+            if (client.getAccount()) {
+                console.log("Account", client.getAccount());
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+
+    //Get AccessToken
+    const tokenResponse = await client.acquireTokenSilent(scopes);
+
+    console.log("Token Response", tokenResponse);
+
+    //Read Profile
+    const qryProfile = "https://graph.microsoft.com/v1.0/me/";
+    const profileResp = await fetch(qryProfile, {
+        headers: {
+            Authorization: "Bearer " + tokenResponse.accessToken,
+        },
     });
-
-  //Get AccessToken
-  const tokenResponse = await client.acquireTokenSilent(scopes);
-
-  console.log("Token Response", tokenResponse);
-
-  //Read Profile
-  const qryProfile = "https://graph.microsoft.com/v1.0/me/";
-  const profileResp = await fetch(qryProfile, {
-    headers: {
-      Authorization: "Bearer " + tokenResponse.accessToken,
-    },
-  });
-  const profile = await profileResp.json();
-  console.log("Profile", profile);
+    const profile = await profileResp.json();
+    console.log("Profile", profile);
 }
