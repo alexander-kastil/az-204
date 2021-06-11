09006d67c24d9d68d93327fc48bc9d1980f04b06
Alexander Pajer <alexander.pajer@integrations.at>
2021-06-11 11:46:11 +0200

    msgq refactor

--- Demos/10-MessageBasedSolutions/MessageQueue/QueueApp/Program.cs
+++ Demos/10-MessageBasedSolutions/MessageQueue/QueueApp/Program.cs
@@ -6,48 +6,46 @@ using Azure.Storage.Queues.Models;
 namespace QueueApp {
     class Program {
 
-        const string connectionString = "DefaultEndpointsProtocol=https;AccountName=msgqueue25387;AccountKey=WIgUpJV9J4g5nJaUJMhd5VQe5/AQBf3yTxezCNrf3GJ8n1J3TykKuZdVBb7vIhus9XF99aMFNCeBe02LDmqNsg==;EndpointSuffix=core.windows.net";
+        const string connectionString = "DefaultEndpointsProtocol=https;EndpointSuffix=core.windows.net;AccountName=msgqueue30925;AccountKey=hbg0bIpCnayj/H5PPsPNLURjYT0jzoOds7+8d14b+2ID3av6zgj41heHYbbhRypnSpUtHi/7CVrfK5pa+p8Wwg==";
 
         static async Task Main (string[] args) {
 
-           QueueClient queue = new QueueClient(connectionString, "az-204-queue");
-            if (false)
-            {
-                var value = "My demo value";
-                await InsertMessageAsync(queue, value);
-                Console.WriteLine($"Sent: {value}");
-            }
-            else
-            {
-                var value = await RetrieveNextMessageAsync(queue);
-                Console.WriteLine($"Received: {value}");
-            }
+            QueueClient queue = new QueueClient(connectionString, "az-204-queue");
+
+            //Insert msg
+            var value = "My demo value";
+            await InsertMessageAsync(queue, value);
+            Console.WriteLine($"Sent: {value}");
+
+
+            var result = await RetrieveNextMessageAsync(queue);
+            Console.WriteLine($"Received: {result}");
 
             Console.Write("Press Enter...");
             Console.ReadLine();
         }
 
-        static async Task InsertMessageAsync(QueueClient theQueue, string newMessage)
+        static async Task InsertMessageAsync(QueueClient q, string msg)
         {
-            if (null != await theQueue.CreateIfNotExistsAsync())
+            if (null != await q.CreateIfNotExistsAsync())
             {
                 Console.WriteLine("The queue was created.");
             }
 
-            await theQueue.SendMessageAsync(newMessage);
+            await q.SendMessageAsync(msg);
         }
 
-        static async Task<string> RetrieveNextMessageAsync(QueueClient theQueue)
+        static async Task<string> RetrieveNextMessageAsync(QueueClient q)
         {
-            if (await theQueue.ExistsAsync())
+            if (await q.ExistsAsync())
             {
-                QueueProperties properties = await theQueue.GetPropertiesAsync();
+                QueueProperties properties = await q.GetPropertiesAsync();
 
                 if (properties.ApproximateMessagesCount > 0)
                 {
-                    QueueMessage[] retrievedMessage = await theQueue.ReceiveMessagesAsync(1);
+                    QueueMessage[] retrievedMessage = await q.ReceiveMessagesAsync(1);
                     string theMessage = retrievedMessage[0].MessageText;
-                    await theQueue.DeleteMessageAsync(retrievedMessage[0].MessageId, retrievedMessage[0].PopReceipt);
+                    await q.DeleteMessageAsync(retrievedMessage[0].MessageId, retrievedMessage[0].PopReceipt);
                     return theMessage;
                 }
                 else
@@ -57,7 +55,7 @@ namespace QueueApp {
 
                     if (response.ToUpper() == "Y")
                     {
-                        await theQueue.DeleteIfExistsAsync();
+                        await q.DeleteIfExistsAsync();
                         return "The queue was deleted.";
                     }
                     else
