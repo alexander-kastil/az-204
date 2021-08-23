3e6327b1e193485c208e43ea1122ab582086da2f
Alexander Pajer <alexander.pajer@integrations.at>
2021-08-23 13:45:20 +0200

    data model

--- Demos/04-FunctionApps/Demo-06/foodpic-approval/model/TeamsApprovalRequest.cs
+++ Demos/04-FunctionApps/Demo-06/foodpic-approval/model/TeamsApprovalRequest.cs
@@ -3,7 +3,7 @@ namespace Integrations
     public class TeamsApprovalRequest{
         public string OrchestrationInstanceID { get; set; }
         public string ReturnUrl { get; set; }
-        public ApprovalRequest request {get;set;}
+        public ApprovalRequest InitialRequest {get;set;}
         public bool Approved { get; set; }
     }
 }
