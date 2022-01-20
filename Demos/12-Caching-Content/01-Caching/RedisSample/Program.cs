7dbb2043ac04d6c4d22cc69db696af21077eb787
Alexander Pajer <alexander.pajer@integrations.at>
2022-01-20 15:20:16 +0100

    redis sample rewrite

--- Demos/12-Caching-Content/01-Caching/RedisSample/Program.cs
+++ Demos/12-Caching-Content/01-Caching/RedisSample/Program.cs
@@ -1,71 +1,47 @@
 ﻿using System;
-using Microsoft.Extensions.Configuration;
 using StackExchange.Redis;
 
-namespace RedisSample {
-
-
-    class Program {
-        static void Main (string[] args) {
-            
-            //dotnet user-secrets set CacheConnection az204-redis-demo-1618.redis.cache.windows.net,abortConnect=false,ssl=true,password="wr988naeX5gM+gPSwJBQGKlQn0idwZJ3LQo++5dfnjE="
-
-            InitializeConfiguration ();
+namespace RedisSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lazy<ConnectionMultiplexer> lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+            {
+                var conStr = "az204-redis-demo-22708.redis.cache.windows.net:6380,password=TaYurPXtWXZ71MeWyyr45YAmv5wdNQo1QAzCaMAbZEU=,ssl=True,abortConnect=False";
+                return ConnectionMultiplexer.Connect(conStr);
+            });
 
             // Connection refers to a property that returns a ConnectionMultiplexer
             // as shown in the previous example.
-            IDatabase cache = lazyConnection.Value.GetDatabase ();
-
-            // Perform cache operations using the cache object...
+            IDatabase cache = lazyConnection.Value.GetDatabase();
 
             // Simple PING command
             string cacheCommand = "PING";
-            Console.WriteLine ("\nCache command  : " + cacheCommand);
-            Console.WriteLine ("Cache response : " + cache.Execute (cacheCommand).ToString ());
+            Console.WriteLine("\nCache command  : " + cacheCommand);
+            Console.WriteLine("Cache response : " + cache.Execute(cacheCommand).ToString());
 
             // Simple get and put of integral data types into the cache
             cacheCommand = "GET Message";
-            Console.WriteLine ("\nCache command  : " + cacheCommand + " or StringGet()");
-            Console.WriteLine ("Cache response : " + cache.StringGet ("Message").ToString ());
+            Console.WriteLine("\nCache command  : " + cacheCommand + " or StringGet()");
+            Console.WriteLine("Cache response : " + cache.StringGet("Message").ToString());
 
             cacheCommand = "SET Message \"Hello! The cache is working from a .NET Core console app!\"";
-            Console.WriteLine ("\nCache command  : " + cacheCommand + " or StringSet()");
-            Console.WriteLine ("Cache response : " + cache.StringSet ("Message", "Hello! The cache is working from a .NET Core console app!").ToString ());
+            Console.WriteLine("\nCache command  : " + cacheCommand + " or StringSet()");
+            Console.WriteLine("Cache response : " + cache.StringSet("Message", "Hello! The cache is working from a .NET Core console app!").ToString());
 
             // Demonstrate "SET Message" executed as expected...
             cacheCommand = "GET Message";
-            Console.WriteLine ("\nCache command  : " + cacheCommand + " or StringGet()");
-            Console.WriteLine ("Cache response : " + cache.StringGet ("Message").ToString ());
+            Console.WriteLine("\nCache command  : " + cacheCommand + " or StringGet()");
+            Console.WriteLine("Cache response : " + cache.StringGet("Message").ToString());
 
             // Get the client list, useful to see if connection list is growing...
             cacheCommand = "CLIENT LIST";
-            Console.WriteLine ("\nCache command  : " + cacheCommand);
-            Console.WriteLine ("Cache response : \n" + cache.Execute ("CLIENT", "LIST").ToString ().Replace ("id=", "id="));
+            Console.WriteLine("\nCache command  : " + cacheCommand);
+            Console.WriteLine("Cache response : \n" + cache.Execute("CLIENT", "LIST").ToString().Replace("id=", "id="));
 
-            lazyConnection.Value.Dispose ();
+            lazyConnection.Value.Dispose();
         }
-
-        private static IConfigurationRoot Configuration { get; set; }
-        const string SecretName = "CacheConnection";
-
-        private static void InitializeConfiguration () {
-            var builder = new ConfigurationBuilder ()
-                .AddUserSecrets<Program> ();
-
-            Configuration = builder.Build ();
-        }
-            
-        private static Lazy<ConnectionMultiplexer> lazyConnection = new Lazy<ConnectionMultiplexer> (() => {
-            string cacheConnection = Configuration[SecretName];
-            Console.WriteLine (cacheConnection);
-            var conStr = "az204-redis-demo-25705.redis.cache.windows.net:6380,password=oKGuCuAvvcAs8J7P3RlNKbxySLD1UNXsHH0+xI+k8wU=,ssl=True,abortConnect=False";
-            return ConnectionMultiplexer.Connect (conStr);
-        });
-
-        public static ConnectionMultiplexer Connection {
-            get {
-                return lazyConnection.Value;
-            }
-    }
     }
 }
