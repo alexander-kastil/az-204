08bde0cd6b83ffcbc750e48c820f29fd0bbd9aaf
Alexander Pajer <alexander.pajer@integrations.at>
2022-01-21 23:46:39 +0100

    processor-v4-notexecuting

---
+++ Setup/Linux/install-function-core-tools.sh
@@ -0,0 +1,4 @@
+wget -q https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get update
+sudo apt-get install azure-functions-core-tools-4
