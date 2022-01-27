22cfd034fd7b70e41d0faf736eea5890586fbbb5
Alexander Pajer <alexander.pajer@integrations.at>
2022-01-27 17:04:56 +0100

    docs

--- Demos/09-EventBasedSolutions/EventGrid/Demo-01/PublishEvtTopic/FoodAppOrderDeliveredEventData.cs
+++
@@ -1,9 +0,0 @@
-using Newtonsoft.Json;
-
-namespace PublishTopic
-{
-    class FoodAppOrderDeliveredEventData {
-        [JsonProperty (PropertyName = "itemSku")]
-        public string OrderNumber { get; set; }
-    }
-}
