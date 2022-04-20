3084d6b1e65c7350c499722fdb13f91a64619c3a
Alexander Pajer <alexander.pajer@integrations.at>
2022-04-20 09:31:25 +0200

    sb rename

---
+++ Demos/10-MessageBasedSolutions/ServiceBus/Demo-02/sb-topic-publisher/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+
+namespace SbApp
+{
+    public class Program
+    {
+        static string connectionString = "Endpoint=sb://foodappdev.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=txqwKdActAoXwS+bY/FVl10W8LRqwMdZykIDezWiD44="; 
+        static string topic = "food-events";
+        private const int numOfMessages = 3;
+
+        public static async Task Main(string[] args)
+        {
+            ServiceBusClient client = new ServiceBusClient(connectionString);
+            ServiceBusSender Sender = client.CreateSender(topic);
+
+            using ServiceBusMessageBatch messageBatch = await Sender.CreateMessageBatchAsync();
+            for (int i = 1; i <= numOfMessages; i++)
+            {
+                if (!messageBatch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
+                {
+                    throw new Exception($"The message {i} is too large to fit in the batch.");
+                }
+            }
+
+            try
+            {
+                await Sender.SendMessagesAsync(messageBatch);
+                Console.WriteLine($"A batch of {numOfMessages} messages has been published to the topic.");
+            }
+            finally
+            {
+                await Sender.DisposeAsync();
+                await client.DisposeAsync();
+            }
+        }
+    }
+}
