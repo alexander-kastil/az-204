0593a97308f874810219363147a4c6b3edd8c2d0
Alexander Pajer <alexander.pajer@integrations.at>
2022-05-05 23:23:59 +0200

    event grid checked

--- Demos/09-EventBasedSolutions/EventGrid/Demo-02/food-orders-ui/src/environments/environment.prod.ts
+++ Demos/09-EventBasedSolutions/EventGrid/Demo-02/food-orders-ui/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  fxEndpoint: 'https://foodorders-7325.azurewebsites.net/api',
+  fxEndpoint: 'https://foodorders-658.azurewebsites.net/api',
 };
--- Setup/wsl/install-az-cli.sh
+++ Setup/wsl/install-az-cli.sh
@@ -1,2 +1,3 @@
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
-
+# install extensions on demandy
+az config set extension.use_dynamic_install=yes
