a11194148317dc0d87d234c36e0784dbfe7dbd87
Alexander Pajer <alexander.pajer@integrations.at>
2022-12-14 16:59:42 +0100

    msg post to fix

--- demos/06-Identity/Demo-06/post-teams-func/postToTeams/index.ts
+++ demos/06-Identity/Demo-06/post-teams-func/postToTeams/index.ts
@@ -1,17 +1,79 @@
 import { AzureFunction, Context, HttpRequest } from '@azure/functions';
+import * as msal from '@azure/msal-node';
+import axios from 'axios';
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    const MS_GRAPH_SCOPE = 'https://graph.microsoft.com/.default';
+    const GRAPH_DEFAULT_SCOPE = 'https://graph.microsoft.com/.default';
 
-    context.log('HTTP trigger function processed a request.');
-    const name = req.query.name || (req.body && req.body.name);
-    const responseMessage = name
-        ? 'Hello, ' + name + '. This HTTP triggered function executed successfully.'
-        : 'This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.';
+    const teamsId = req.body && req.body.teamsId;
+    const channelId = req.body && req.body.channelId;
+    const msg = req.body && req.body.message;
+
+    if (!teamsId || !msg) {
+        context.res = {
+            status: 400,
+            body: 'Please pass a teamid and message in the request body',
+        };
+        return;
+    }
+
+    const msalConfig = {
+        auth: {
+            clientId: process.env['clientID'],
+            authority: 'https://login.microsoftonline.com/' + process.env['tenantID'],
+            clientSecret: process.env['secret'],
+        },
+    };
+
+    const app = new msal.ConfidentialClientApplication(msalConfig);
+
+    if (app) {
+        const redirectUri = process.env['redirectUri'];
+
+        const clientCredentialRequest = {
+            scopes: [GRAPH_DEFAULT_SCOPE],
+            resourceRequestUri: redirectUri,
+        };
+
+        const token = await app.acquireTokenByClientCredential(clientCredentialRequest);
+
+        const req = {
+            body: {
+                content: msg,
+            },
+        };
+
+        const url = 'https://graph.microsoft.com/v1.0/teams/' + teamsId + '/channels/' + channelId + '/messages';
+
+        const resp = await axios.post(url, req, {
+            headers: {
+                Authorization: `Bearer ${token.accessToken}`,
+            },
+        });
+
+        // let resp = await app.getAuthCodeUrl(authCodeUrlParameters).catch((error) => console.log(JSON.stringify(error)));
+
+        // console.log('response', resp);
+
+        // const tokenRequest = {
+        //     code: 'authorization_code',
+        //     redirectUri: redirectUri,
+        //     scopes: [GRAPH_DEFAULT_SCOPE],
+        // };
+
+        // app.acquireTokenByCode(tokenRequest)
+        //     .then((response) => {
+        //         console.log('\nResponse: \n:', response);
+        //     })
+        //     .catch((error) => {
+        //         console.log(error);
+        //     });
+
+        // console.log('token', token);
+    }
 
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage,
+        status: 200,
     };
 };
 
