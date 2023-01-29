52be1453022b9d193160f9f8a5921cfe75884d58
Alexander Pajer <alexander.pajer@integrations.at>
2023-01-29 13:56:31 +0100

    queue base64 encoding

--- demos/10-message-based-solutions/01-message-queues/demo-01/queue-producer/Program.cs
+++ demos/10-message-based-solutions/01-message-queues/demo-01/queue-producer/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.IO;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Azure.Storage.Queues;
-using Azure.Storage.Queues.Models;
 using Microsoft.Extensions.Configuration;
 
 namespace QueueApp
 {
+
     class Program
     {
         static async Task Main(string[] args)
@@ -18,59 +20,26 @@ namespace QueueApp
             var connectionString = configuration["msgqConStr"];
             var q = configuration["queueName"];
             QueueClient queue = new QueueClient(connectionString, q);
-            
-            for (int i = 0; i < 100; i++)
+
+            for (int i = 0; i < 10; i++)
             {
-                var value = "${'id': " + i + ", 'name': 'name" + i + "'}";
-                await InsertMessageAsync(queue, value);
+                var item = new OrderItem { Id = i, Product = "Product " + i, Quantity = i };
+                await InsertMessageAsync(queue, item);
                 System.Threading.Thread.Sleep(10);
-                Console.WriteLine($"Sent: {value}");
             }
         }
 
-        static async Task InsertMessageAsync(QueueClient q, string msg)
+        static async Task InsertMessageAsync(QueueClient q, OrderItem item)
         {
             if (null != await q.CreateIfNotExistsAsync())
             {
                 Console.WriteLine("The queue was created.");
             }
 
+            // QueueTrigger expects a base64 encoded string
+            var msg = Convert.ToBase64String(Encoding.UTF8.GetBytes(JsonSerializer.Serialize(item)));
             await q.SendMessageAsync(msg);
+            Console.WriteLine($"Sent: {item.Product}");
         }
-
-        // static async Task<string> RetrieveNextMessageAsync(QueueClient q)
-        // {
-        //     if (await q.ExistsAsync())
-        //     {
-        //         QueueProperties properties = await q.GetPropertiesAsync();
-
-        //         if (properties.ApproximateMessagesCount > 0)
-        //         {
-        //             QueueMessage[] retrievedMessage = await q.ReceiveMessagesAsync(1);
-        //             string theMessage = retrievedMessage[0].MessageText;
-        //             await q.DeleteMessageAsync(retrievedMessage[0].MessageId, retrievedMessage[0].PopReceipt);
-        //             return theMessage;
-        //         }
-        //         else
-        //         {
-        //             Console.Write("The queue is empty. Attempt to delete it? (Y/N) ");
-        //             string response = Console.ReadLine();
-
-        //             if (response.ToUpper() == "Y")
-        //             {
-        //                 await q.DeleteIfExistsAsync();
-        //                 return "The queue was deleted.";
-        //             }
-        //             else
-        //             {
-        //                 return "The queue was not deleted.";
-        //             }
-        //         }
-        //     }
-        //     else
-        //     {
-        //         return "The queue does not exist. Add a message to the command line to create the queue and store the message.";
-        //     }
-        // }
     }
 }
