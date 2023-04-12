71eb1d6b57a464e5105f519a4bbd297ac395966a
Alexander Pajer <alexander.pajer@integrations.at>
2023-04-12 08:16:32 +0200

    setup

--- setup/windows-subsystem-linux/all-in-one.sh
+++ setup/windows-subsystem-linux/all-in-one.sh
@@ -1,16 +1,16 @@
 sudo apt update
 
-# node
-sudo curl -sL https://deb.nodesource.com/setup_14.x | sudo bash
-sudo apt-get install -y nodejs
+# node & azure cli
+sudo apt-get install curl
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+sudo apt-get install build-essential
+nvm install 16.15.0
+nvm use 16.15.0
+npm install -g @angular/cli
 
 # .net
 wget https://packages.microsoft.com/config/ubuntu/20.10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
-
-sudo apt-get install -y apt-transport-https && \
-sudo apt-get update && \
-sudo apt-get install -y dotnet-sdk-3.1
 
 sudo apt-get install -y apt-transport-https && \
 sudo apt-get update && \
@@ -19,6 +19,3 @@ sudo apt-get install -y dotnet-sdk-6.0
 # azure cli
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 az config set extension.use_dynamic_install=yes
-
-# function core tools
-sudo npm install -g azure-functions-core-tools@4 --unsafe-perm true
