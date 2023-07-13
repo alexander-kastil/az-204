3e2b466b929f0b831326a17815e1b476604b2413
Alexander Pajer <alexander.pajer@integrations.at>
2023-07-13 08:59:02 +0200

    container to acr (#155)

--- app/food-shop-ui/src/app/about/about.component.ts
+++ app/food-shop-ui/src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { environment } from 'src/environments/environment';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-about',
