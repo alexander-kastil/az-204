2e6087213eb265fd92003c7a593bc1a268ffeff0
Alexander Pajer <alexander.pajer@integrations.at>
2023-07-21 09:54:12 +0200

    post working

--- demos/03-cosmos-db/demo-03/food-order-service/Common/Order.Model.cs
+++ demos/03-cosmos-db/demo-03/food-order-service/Common/Order.Model.cs
@@ -3,27 +3,25 @@ using System.Collections.Generic;
 
 namespace FoodApp.Orders
 {
-    public interface Order{
-        public string Id {get;set;}
+    public class Order{
+        public int Id {get;set;}
         public decimal Total {get;set;}
         public Customer Customer {get;set;}
         public List<OrderItem> Items {get;set;}
-        public OrderStatus Status {get;set;}
     }
 
-    public interface Customer{
+    public class Customer{
         public string Name {get;set;}
         public string EMail {get;set;}
         public string Address {get;set;}
     }
     
-    public interface OrderItem
+    public class OrderItem
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
         public int Quantity { get; set; }
-        public string OrchestrationInstanceId { get; set; }
     }
 
     public enum OrderStatus
