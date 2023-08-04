d0c2a077c65b9145f5d4b0c5a8f3bdd7d969083d
Alexander Pajer <alexander.pajer@integrations.at>
2023-08-04 11:32:26 +0200

    q consumer (#188)

--- demos/10-message-based-solutions/01-message-queues/demo-01/queue-consumer-funct/OrderItem.cs
+++
@@ -1,9 +0,0 @@
-namespace QueueApp
-{
-    public class OrderItem
-    {
-        public int Id { get; set; }
-        public int Quantity { get; set; }
-        public string Product { get; set; }
-    }
-}
