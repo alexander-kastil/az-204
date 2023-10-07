d729352632a620cde3eaba38d38b0f4bf2f0b7a6
Alexander Pajer <alexander.pajer@integrations.at>
2023-10-07 14:04:29 +0200

    net installation

--- setup/windows-subsystem-linux/install-net.sh
+++ setup/windows-subsystem-linux/install-net.sh
@@ -1,12 +1,3 @@
-wget https://packages.microsoft.com/config/ubuntu/20.10/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-sudo dpkg -i packages-microsoft-prod.deb
-
-sudo apt-get update; \
-sudo apt-get install -y apt-transport-https && \
-sudo apt-get update && \
-sudo apt-get install -y dotnet-sdk-3.1
-
-sudo apt-get update; \
-sudo apt-get install -y apt-transport-https && \
-sudo apt-get update && \
-sudo apt-get install -y dotnet-sdk-6.0
+sudo rm /etc/apt/sources.list.d/microsoft-prod.list
+sudo apt update
+sudo apt install dotnet-sdk-7.0
