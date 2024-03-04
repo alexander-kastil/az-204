239a018144a8ea091605d4df00403dc8a363fcd6
Alexander Pajer <alexander.pajer@integrations.at>
2024-03-04 07:19:25 +0100

    refactor to binder missing https://github.com/arambazamba/az-204//blob/func-upload/demos/10-message-based-solutions/01-message-queues/demo-02/food-invoices/Invoices/GenerateInvoice.cs

--- demos/04-function-apps/demo-03/text-to-storage-java/src/main/java/com/function/Function.java
+++ demos/04-function-apps/demo-03/text-to-storage-java/src/main/java/com/function/Function.java
@@ -5,38 +5,33 @@ import com.microsoft.azure.functions.HttpMethod;
 import com.microsoft.azure.functions.HttpRequestMessage;
 import com.microsoft.azure.functions.HttpResponseMessage;
 import com.microsoft.azure.functions.HttpStatus;
+import com.microsoft.azure.functions.OutputBinding;
 import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.BlobOutput;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
-
-import java.util.Optional;
 
 /**
  * Azure Functions with HTTP Trigger.
  */
 public class Function {
-    /**
-     * This function listens at endpoint "/api/HttpExample". Two ways to invoke it using "curl" command in bash:
-     * 1. curl -d "HTTP Body" {your host}/api/HttpExample
-     * 2. curl "{your host}/api/HttpExample?name=HTTP%20Query"
-     */
+
     @FunctionName("UploadNewsletter")
     public HttpResponseMessage run(
-            @HttpTrigger(
-                name = "req",
-                methods = {HttpMethod.POST},
-                authLevel = AuthorizationLevel.ANONYMOUS)
-                HttpRequestMessage<String> request,
-            final ExecutionContext context) {
-        context.getLogger().info("Java HTTP trigger processed a request.");
+            @HttpTrigger(name = "req", methods = {
+                    HttpMethod.POST }, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<String> request,
+            @BlobOutput(name = "outputBlob", path = "newsletters/{blobName}", connection = "StorageConnection") OutputBinding<String> outputBlob,
+        final ExecutionContext context) {
+                context.getLogger().info("Java HTTP trigger processed a request.");
 
-        // Parse query parameter
-        final String name = request.getBody();
+                // Parse query parameter
+                final String text = request.getBody();
+                String fileName = "newsletter-" + java.util.UUID.randomUUID() + ".txt";
 
-        if (name == null) {
-            return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
-        } else {
-            return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name).build();
+                outputBlob.setValue(text);
+
+                return request.createResponseBuilder(HttpStatus.OK)
+                                .body("Text file uploaded successfully!")
+                                .build();
         }
-    }
 }
