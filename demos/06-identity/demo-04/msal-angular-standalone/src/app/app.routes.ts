3e81799eb9280ba3d90413256808113a13ffcd7d
Alexander Pajer <alexander.pajer@integrations.at>
2024-03-04 08:08:36 +0100

    msal angular standalone

---
+++ demos/06-identity/demo-04/msal-angular-standalone/src/app/app.routes.ts
@@ -0,0 +1,21 @@
+import { Route } from '@angular/router';
+import { MsalGuard } from '@azure/msal-angular';
+import { FailedComponent } from './failed/failed.component';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+
+export const appRoutes: Route[] = [
+    {
+        path: 'profile',
+        component: ProfileComponent,
+        canActivate: [MsalGuard]
+    },
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'login-failed',
+        component: FailedComponent
+    }
+];
