6679c1a41989b5f48696b617b7cceb48f6d6170f
Alexander Pajer <alexander.pajer@integrations.at>
2024-03-04 06:30:26 +0100

    func rename

--- demos/04-function-apps/demo-03/text-to-storage-java/src/main/java/com/function/Function.java
+++ demos/04-function-apps/demo-03/text-to-storage-java/src/main/java/com/function/Function.java
@@ -20,7 +20,7 @@ public class Function {
      * 1. curl -d "HTTP Body" {your host}/api/HttpExample
      * 2. curl "{your host}/api/HttpExample?name=HTTP%20Query"
      */
-    @FunctionName("HttpExample")
+    @FunctionName("UploadNewsletter")
     public HttpResponseMessage run(
             @HttpTrigger(
                 name = "req",
