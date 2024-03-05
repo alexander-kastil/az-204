1b4e20ec8a00d58e888a3334bd80741cde9776a9
Alexander Pajer <alexander.pajer@integrations.at>
2024-03-05 08:36:21 +0100

    env

--- demos/04-function-apps/demo-03/text-to-storage-java/src/main/java/com/function/Function.java
+++ demos/04-function-apps/demo-03/text-to-storage-java/src/main/java/com/function/Function.java
@@ -1,5 +1,9 @@
 package com.function;
 
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.BlobServiceClientBuilder;
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.HttpMethod;
 import com.microsoft.azure.functions.HttpRequestMessage;
@@ -10,28 +14,40 @@ import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.BlobOutput;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
+import io.github.cdimascio.dotenv.Dotenv;
 
 /**
  * Azure Functions with HTTP Trigger.
  */
 public class Function {
 
-    @FunctionName("UploadNewsletter")
-    public HttpResponseMessage run(
-            @HttpTrigger(name = "req", methods = {
-                    HttpMethod.POST }, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<String> request,
-            @BlobOutput(name = "outputBlob", path = "newsletters/{blobName}", connection = "StorageConnection") OutputBinding<String> outputBlob,
-        final ExecutionContext context) {
+        @FunctionName("UploadNewsletter")
+        public HttpResponseMessage run(
+                @HttpTrigger(name = "req", methods = {
+                                HttpMethod.POST }, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<String> request,
+                final ExecutionContext context) {
                 context.getLogger().info("Java HTTP trigger processed a request.");
 
                 // Parse query parameter
                 final String text = request.getBody();
                 String fileName = "newsletter-" + java.util.UUID.randomUUID() + ".txt";
+                
+                // Load environment variables
+                Dotenv dotenv = Dotenv.load();
+                String connectionString = dotenv.get("CONNECTION_STRING");
+                String containerName = dotenv.get("CONTAINER_NAME");
 
-                outputBlob.setValue(text);
+                // Create a BlobServiceClient object which will be used to create a container client
+                BlobServiceClient blobServiceClient = new BlobServiceClientBuilder().connectionString(connectionString).buildClient();
 
-                return request.createResponseBuilder(HttpStatus.OK)
-                                .body("Text file uploaded successfully!")
-                                .build();
+                // Create the container and return a container client object
+                BlobContainerClient containerClient = blobServiceClient.getBlobContainerClient(containerName);
+
+                // Get a reference to a blob
+                BlobClient blobClient = containerClient.getBlobClient(fileName);
+
+                // Upload the blob
+                blobClient.uploadFromFile(text);
+                return request.createResponseBuilder(HttpStatus.OK).body("File uploaded successfully").build();
         }
 }
