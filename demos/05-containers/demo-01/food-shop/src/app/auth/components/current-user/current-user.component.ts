24a90bb77e3beda4dd6572046ebd374ffc1616bc
Alexander Pajer <alexander.pajer@integrations.at>
2024-03-25 12:51:06 +0100

    cleanup

--- demos/05-containers/demo-01/food-shop/src/app/auth/components/current-user/current-user.component.ts
+++ demos/05-containers/demo-01/food-shop/src/app/auth/components/current-user/current-user.component.ts
@@ -1,16 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MsalAuthFacade } from '../../state/auth.facade';
 import { AsyncPipe } from '@angular/common';
 
 @Component({
-    selector: 'app-current-user',
-    templateUrl: './current-user.component.html',
-    styleUrls: ['./current-user.component.scss'],
-    standalone: true,
-    imports: [AsyncPipe],
+  selector: 'app-current-user',
+  templateUrl: './current-user.component.html',
+  styleUrls: ['./current-user.component.scss'],
+  standalone: true,
+  imports: [AsyncPipe],
 })
 export class CurrentUserComponent {
-  user = this.af.getUser();
-
-  constructor(private af: MsalAuthFacade) {}
+  auth = inject(MsalAuthFacade);
+  user = this.auth.getUser();
 }
