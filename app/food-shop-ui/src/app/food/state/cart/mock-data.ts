5175919fe966042fb1d9df7b626ab21e60c42036
Alexander Pajer <alexander.pajer@integrations.at>
2024-03-26 06:40:46 +0100

    cleanup (#221)

--- app/food-shop-ui/src/app/food/state/cart/mock-data.ts
+++
@@ -1,7 +0,0 @@
-
-export const mockOrder = {
-  customer: { id: 'cysdss', name: 'Alexander Pajer', address: 'Hauptstraße 1, Wien, Austria', email: 'alexander.pajer@integrations.at' },
-  payment: { type: 'Credit Card', account: '**** **** **** 1234' },
-  items: [],
-  status: 'cart'
-}
