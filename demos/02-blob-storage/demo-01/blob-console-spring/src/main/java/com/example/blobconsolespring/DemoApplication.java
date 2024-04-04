54ca668ebed8db2f369fccd23926d0bb1851ecce
Alexander Pajer <alexander.pajer@integrations.at>
2024-04-04 08:47:10 +0200

    spring blob

--- demos/02-blob-storage/demo-01/blob-console-spring/src/main/java/com/example/blobconsolespring/DemoApplication.java
+++ demos/02-blob-storage/demo-01/blob-console-spring/src/main/java/com/example/blobconsolespring/DemoApplication.java
@@ -21,18 +21,19 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 
 		try {
-			String connectionString = "DefaultEndpointsProtocol=https;EndpointSuffix=core.windows.net;AccountName=foodpicsdev;AccountKey=BV1X0wCSWwfrekNd8sbX8J+JGhakQjpgdx14P/OojJhqr0gWq/jVzLDS2DhgqGMa7q+606ga0lLZ+ASt+6ICfA==;BlobEndpoint=https://foodpicsdev.blob.core.windows.net/;FileEndpoint=https://foodpicsdev.file.core.windows.net/;QueueEndpoint=https://foodpicsdev.queue.core.windows.net/;TableEndpoint=https://foodpicsdev.table.core.windows.net/";
+			String connectionString = "<CONNECTION_STRING>";
+			String containerName = "food-pics";
+			String relativePath = "../food-pics";
 
-			// Create a BlobServiceClient object which will be used to create a container client
-			BlobServiceClient blobServiceClient = new BlobServiceClientBuilder().connectionString(connectionString).buildClient();
-
-			//Create a unique name for the container
-			String containerName = "food-" + java.util.UUID.randomUUID();
+			// Create a BlobServiceClient object which will be used to create a container
+			// client
+			BlobServiceClient blobServiceClient = new BlobServiceClientBuilder().connectionString(connectionString)
+					.buildClient();
 
 			// Create the container and return a container client object
 			BlobContainerClient containerClient = blobServiceClient.createBlobContainer(containerName);
 			// Get the path to the image directory
-			Path path = Paths.get("../food-pics");
+			Path path = Paths.get(relativePath).toAbsolutePath().normalize();
 
 			// Check if directory exists
 			if (!Files.exists(path)) {
@@ -42,32 +43,32 @@ public class DemoApplication {
 
 			try (Stream<Path> paths = Files.walk(path)) {
 				paths
-					// Filter out directories, we want files only
-					.filter(Files::isRegularFile)
-					// For each file
-					.forEach(filePath -> {
-						// Get the filename
-						String filename = filePath.getFileName().toString();
+						// Filter out directories, we want files only
+						.filter(Files::isRegularFile)
+						// For each file
+						.forEach(filePath -> {
+							// Get the filename
+							String filename = filePath.getFileName().toString();
 
-						// Check if the file is an image
-						if (filename.endsWith(".jpg") || filename.endsWith(".png") || filename.endsWith(".jpeg")) {
-							System.out.println("Uploading " + filename);
+							// Check if the file is an image
+							if (filename.endsWith(".jpg") || filename.endsWith(".png") || filename.endsWith(".jpeg")) {
+								System.out.println("Uploading " + filename);
 
-							// Get a blob client for the file
-							BlobClient blobClient = containerClient.getBlobClient(filename);
+								// Get a blob client for the file
+								BlobClient blobClient = containerClient.getBlobClient(filename);
 
-							// Upload the file
-							try {
-								blobClient.uploadFromFile(filePath.toString(), true);
-							} catch (Exception e) {
-								System.out.println("Error uploading file " + filename + ": " + e.getMessage());
+								// Upload the file
+								try {
+									blobClient.uploadFromFile(filePath.toString(), true);
+								} catch (Exception e) {
+									System.out.println("Error uploading file " + filename + ": " + e.getMessage());
+								}
 							}
-						}
-					});
+						});
 			} catch (IOException e) {
 				System.out.println("Error reading files: " + e.getMessage());
 			}
-			System.out.println("Connected to Azure Blob Storage");
+			System.out.println("Uploaded all files to Azure Blob Storage");
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
